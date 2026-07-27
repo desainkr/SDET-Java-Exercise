5c668d33efecdb4a27b7184d27f703282c61c51b
desainkr <desainkr@gmail.com>
2026-07-27 08:15:39 -0700

    This is second commit

---
+++ CICIDTest.java
@@ -0,0 +1,10 @@
+package SDETInterview;
+
+public class CICIDTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- StaicNosttaic.java
+++ StaicNosttaic.java
@@ -1,6 +1,6 @@
 package SDETInterview;
 
-public class Counter {
+public class StaicNosttaic {
 	public static int staticCount = 0; // Shared across all instances
 	public int nonStaticCount = 0; // Unique per instance
 
@@ -13,17 +13,17 @@ public class Counter {
 	}
 
 	public static void main(String[] args) {
-		Counter c1 = new Counter();
-		Counter c2 = new Counter();
+		StaicNosttaic c1 = new StaicNosttaic();
+		StaicNosttaic c2 = new StaicNosttaic();
 
-		Counter.incrementStatic(); // staticCount = 1
-		Counter.incrementStatic(); // staticCount = 2
-		Counter.incrementStatic(); // staticCount = 3
+		StaicNosttaic.incrementStatic(); // staticCount = 1
+		StaicNosttaic.incrementStatic(); // staticCount = 2
+		StaicNosttaic.incrementStatic(); // staticCount = 3
 
 		c1.incrementNonStatic(); // c1.nonStaticCount = 1
 		c2.incrementNonStatic(); // c2.nonStaticCount = 1
 
-		System.out.println("Static Count = " + Counter.staticCount);
+		System.out.println("Static Count = " + StaicNosttaic.staticCount);
 		System.out.println("c1 Non-Static Count = " + c1.nonStaticCount);
 		System.out.println("c2 Non-Static Count = " + c2.nonStaticCount);
 	}
